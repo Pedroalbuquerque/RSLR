c63b29771e76fd3c6b709859ae6007168814ff19
luisr320 <luisr320@gmail.com>
2016-07-08 13:00:12 +0100

    Create a #define for using Adafruit GPS

--- RSLR.ino
+++ RSLR.ino
@@ -22,6 +22,7 @@
 #define FREQUENCY   434 //Match frequency to the hardware version of the radio on your Moteino
 #define LED           9 // Moteinos have LEDs on D9
 #define FLASH_SS      8 // and FLASH SS on D8
+//#define ADAFRUITGPS     // uncomment if you're using Adafruit's Ultimate GPS Breakout
 
 bool LEDstatus = false;
 
@@ -101,20 +102,23 @@ void setup()
 	// 9600 NMEA is the default baud rate for Adafruit MTK GPS's- some use 4800
 	GPS.begin(9600);
 
-	// uncomment this line to turn on RMC (recommended minimum) and GGA (fix data) including altitude
-	GPS.sendCommand(PMTK_SET_NMEA_OUTPUT_RMCGGA);
-	// uncomment this line to turn on only the "minimum recommended" data
-	//GPS.sendCommand(PMTK_SET_NMEA_OUTPUT_RMCONLY);
-	// For parsing data, we don't suggest using anything but either RMC only or RMC+GGA since
-	// the parser doesn't care about other sentences at this time
+#ifdef ADAFRUITGPS
+		//*****uncomment this line to turn on RMC (recommended minimum) and GGA (fix data) including altitude****
+		GPS.sendCommand(PMTK_SET_NMEA_OUTPUT_RMCGGA);
+		// uncomment this line to turn on only the "minimum recommended" data
+		//GPS.sendCommand(PMTK_SET_NMEA_OUTPUT_RMCONLY);
+		// For parsing data, we don't suggest using anything but either RMC only or RMC+GGA since
+		// the parser doesn't care about other sentences at this time
 
-	// Set the update rate
-	GPS.sendCommand(PMTK_SET_NMEA_UPDATE_1HZ);   // 1 Hz update rate
-	// For the parsing code to work nicely and have time to sort thru the data, and
-	// print it out we don't suggest using anything higher than 1 Hz
+		//*****Set the update rate*****
+		GPS.sendCommand(PMTK_SET_NMEA_UPDATE_1HZ);   // 1 Hz update rate
+		// For the parsing code to work nicely and have time to sort thru the data, and
+		// print it out we don't suggest using anything higher than 1 Hz
 
-	// Request updates on antenna status, comment out to keep quiet
-	//GPS.sendCommand(PGCMD_ANTENNA);
+		// Request updates on antenna status, comment out to keep quiet
+		//GPS.sendCommand(PGCMD_ANTENNA);
+#endif
+
 
 	// the nice thing about this code is you can have a timer0 interrupt go off
 	// every 1 millisecond, and read data from the GPS for you. that makes the
